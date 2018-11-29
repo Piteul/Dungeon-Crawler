bc969f21484ce00e52a3166492cafeb9604cc76e
Piteul <axel.tetart@gmail.com>
2018-11-29 09:35:51 +0100

    Modif Player Script
    Change the movement speed
    Replace FixedUpdate by Update -> Movement more fluid

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -47,7 +47,7 @@ public class PlayerScript : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void FixedUpdate() {
+    void Update() {
         playerMovement();
         Attack();
     }
