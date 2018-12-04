15a1a880fb171fb367fea8f1042fe0cc10ba928c
Piteul <axel.tetart@gmail.com>
2018-12-04 18:02:21 +0100

    New scene

---
+++ Assets/Floor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+    public List<Sprite> sprites;
+
+    // Use this for initialization
+    void Start() {
+
+        //Debug.Log(sprites.Count);
+        if (sprites.Count > 0) {
+            this.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count)];
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
